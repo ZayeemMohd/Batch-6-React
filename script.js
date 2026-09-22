6d1a6b47f76957f19b526a9d4fefdbbf7533d702
Zayeem Mohd <zayeemmohd@Zayeems-MacBook-Pro-2.local>
2026-09-22 14:42:35 +0530

    React Day 0

--- script.js
+++ script.js
@@ -5,21 +5,24 @@
 
 // document.querySelector("body").insertAdjacentElement("beforeend", h1Elem)
 
-
-    // <div>
-    //     <h1></h1>
-    //     <h2></h2>
-    // </div>
+// <div>
+//     <h1></h1>
+//     <h2></h2>
+//     <div> 
+//             <h1> </h1> 
+//             <h2> </h2>
+//      </div>
+// </div>
 
 // tariqa no: 3
-const h1Elem = React.createElement("h1", {id: "h1-react"}, "Hello world from react");
+const parent = React.createElement("div", {}, [
+  React.createElement("h1", { id: "h1-react" }, "Hello world from react"),
+  React.createElement("h2", {id: "h2-react"}, "Hello h2"),
+  React.createElement("div", {id: "child-div", class: "child"}, [
+    React.createElement("h1", {}, "I am h1 of child-div"),
+    React.createElement('h2', {}, "I am h2 of child-div")
+  ])
+]);
 
-const h2Elem = React.createElement("h2", {}, "Hello h2")
-
-const parent = React.createElement("div", {}, [h1Elem, h2Elem])
-
-
-
-
-let root = ReactDOM.createRoot(document.querySelector("#root"))
-root.render(parent)
+let root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(parent);
